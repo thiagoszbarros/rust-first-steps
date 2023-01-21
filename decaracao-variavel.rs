193e1a523e919790f9fc72c98bd3a67adb087240
Thiago Barros <thiago.barros@bobby.com.br>
2023-01-21 14:49:05 -0300

    refactor: importa modulo exter para obter tipo de variavel

--- decaracao-variavel.rs
+++ decaracao-variavel.rs
@@ -1,13 +1,11 @@
-use std::any::type_name;
 
-fn type_of<T>(_: T) -> &'static str {
-    type_name::<T>()
-}
+mod tipo_da_variavel;
 
 fn main(){
 	let variavel:u8 = 128;
 	println!("variavel = {}, tamanho em byte = {}, tipo = {}", 
 	variavel, 
 	std::mem::size_of_val(&variavel), 
-	type_of(variavel))
+	tipo_da_variavel::tipo(variavel))
 }
+
