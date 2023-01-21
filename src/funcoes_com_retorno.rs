f2d2390f7b9d827d3b537f516c278970e43ae663
Thiago Barros <thiagobarros95@gmail.com>
2023-01-21 16:44:52 -0300

    feat: adiciona funcoes com retorno

---
+++ src/funcoes_com_retorno.rs
@@ -0,0 +1,8 @@
+fn soma(a: i32, b: i32) -> i32 {
+    //obrigatoriamente uma funcao que retorno algo deve ter este retorno tipado
+    a + b //aqui o uso do return sera opcional
+}
+
+fn main() {
+    println!("{} + {} = {}", 2, 2, soma(2, 2));
+}
