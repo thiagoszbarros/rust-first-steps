a0862915b7decb853c4a51fbc89dbc03f1c2701f
Thiago Barros <thiagobarros95@gmail.com>
2023-01-21 15:32:08 -0300

    fix: corrige nome do modulo

--- decaracao-variavel.rs
+++ decaracao-variavel.rs
@@ -1,33 +1,33 @@
 
-#[path = "./utils/tipo_da_variavel.rs"] mod tipo_da_variavel;
+#[path = "./utils/obter-tipo.rs"] mod obter_tipo;
 
 fn main(){
 	let numero_inteiro:u8 = 128;
 	println!("variavel = {}, tamanho em byte = {}, tipo = {}", 
 	numero_inteiro, 
 	std::mem::size_of_val(&numero_inteiro), 
-	tipo_da_variavel::tipo(numero_inteiro));
+	obter_tipo::tipo(numero_inteiro));
 	
 	let numero_decimal:f32 = 2.5;
 	println!("variavel = {}, tamanho em byte = {}, tipo = {}", 
 	numero_decimal, 
 	std::mem::size_of_val(&numero_decimal), 
-	tipo_da_variavel::tipo(numero_decimal));
+	obter_tipo::tipo(numero_decimal));
 
 	let letra:char = 'R';
 	println!("variavel = {}, tamanho em byte = {}, tipo = {}", 
 	letra, 
 	std::mem::size_of_val(&letra), 
-	tipo_da_variavel::tipo(letra));
+	obter_tipo::tipo(letra));
 
 	let mut booleano:bool = true; //por padrao no rust as variaveis sao imutaveis, a palavra reservada mut permite a reatribuicao do valor da variavel
 	println!("variavel = {}, tamanho em byte = {}, tipo = {}", 
 	booleano, 
 	std::mem::size_of_val(&booleano), 
-	tipo_da_variavel::tipo(booleano));
+	obter_tipo::tipo(booleano));
 	booleano = false;
 	println!("Novo valor da variavel mutavel = {}, tamanho em byte = {}, tipo = {}", 
 	booleano, 
 	std::mem::size_of_val(&booleano), 
-	tipo_da_variavel::tipo(booleano))
+	obter_tipo::tipo(booleano))
 }
