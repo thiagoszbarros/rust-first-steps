785cfee05c630b61e8985956a0e999192965389c
Thiago Barros <thiagobarros95@gmail.com>
2023-01-21 16:53:07 -0300

    feat: adiciona comentario pertinentes as funcoes com retorno

--- src/funcoes_com_retorno.rs
+++ src/funcoes_com_retorno.rs
@@ -1,6 +1,11 @@
+//obrigatoriamente uma funcao que retorno algo deve ter este retorno tipado
+//aqui o uso do return sera opcional
+//para que o valor de a + b seja retornado nao deve ser utilizado o ";"
+//o ";" identifica o trecho de código com um expressao
+//usar o ";" fara a funcao retornar () enquanto espera retornar um i32
+
 fn soma(a: i32, b: i32) -> i32 {
-    //obrigatoriamente uma funcao que retorno algo deve ter este retorno tipado
-    a + b //aqui o uso do return sera opcional
+    a + b
 }
 
 fn main() {
