5c5a6ac498788c79d380f0fed58021076a7eaa4e
Thiago Barros <thiagobarros95@gmail.com>
2023-01-21 15:02:13 -0300

    fix: altera nome da variavel deixando mais explicita

--- decaracao-variavel.rs
+++ decaracao-variavel.rs
@@ -2,9 +2,10 @@
 #[path = "./utils/tipo_da_variavel.rs"] mod tipo_da_variavel;
 
 fn main(){
-	let variavel:u8 = 128;
+	let numero_inteiro:u8 = 128;
 	println!("variavel = {}, tamanho em byte = {}, tipo = {}", 
-	variavel, 
-	std::mem::size_of_val(&variavel), 
-	tipo_da_variavel::tipo(variavel))
+	numero_inteiro, 
+	std::mem::size_of_val(&numero_inteiro), 
+	tipo_da_variavel::tipo(numero_inteiro))
+
 }
