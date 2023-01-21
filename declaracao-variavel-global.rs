0bd67c6b600d2f6855e730405a2bd39ea7ebdc3d
Thiago Barros <thiagobarros95@gmail.com>
2023-01-21 15:52:09 -0300

    feat: adiciona declaracao de variavel global

---
+++ declaracao-variavel-global.rs
@@ -0,0 +1,15 @@
+#[path = "./utils/obter-tipo.rs"] mod obter_tipo;
+
+static VARIAVEL_GLOBAL:&str = "Variavel global"; 
+// static mut VARIAVEL_GLOBAL:&str = "Variavel global"; 
+//por padrao, o rust nao permite o uso de variaveis globais mutaveis 
+//para ignorar isso se faz necessario utilizar a anotacao unfase{} para definir o bloco de codigo mo inseguro
+
+fn main(){
+	//unsafe{
+		println!("variavel global = {}, tamanho em byte = {}, tipo = {}", 
+		VARIAVEL_GLOBAL, 
+		std::mem::size_of_val(&VARIAVEL_GLOBAL), 
+		obter_tipo::tipo(VARIAVEL_GLOBAL));
+	//}
+}
